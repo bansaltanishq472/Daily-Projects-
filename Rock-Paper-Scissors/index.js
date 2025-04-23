cb4f4617e81b3b32acc689655b7994319566bbff
Tanishq Bansal <t.bansal@tcog.in>
2025-04-23 23:51:40 +0530

    Added the CSS and Added the button functions and Some JS

---
+++ Rock-Paper-Scissors/index.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll("button");
+const resultEl = document.getElementById("result");
+const playerScoreEl = document.getElementById("user-score");
+const computerScoreEl = document.getElementById("computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const result = playRound(button.id, computerPlay());
+        resultEl.textContent = result;
+    })
+})
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+
