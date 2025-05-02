2fbb11bf54c18c676d7a15b28d25735a7e6dec7c
Tanishq Bansal <t.bansal@tcog.in>
2025-05-02 23:47:28 +0530

    Update the learning

--- LearningTopics/index.js
+++ LearningTopics/index.js
@@ -3,3 +3,18 @@
 // Callback Hell And Event Loop
 // Promises
 // Async Await Syntax
+// Hoisting
+// Hosting
+// What is an array, and how to access its elements?
+// Difference between == and ===
+// Purpose of the isNaN function
+// What is null and undefined ?
+// Purpose of map methood in JS
+// Event Bubbling and event capturing
+// What are higher-order function with examples ?
+// What is an IIFE (Immediately Invoked Function Expression)?
+// Explain closures in JavaScript.
+// How do setTimeout and setInterval work?
+// Describe promises in Javascript.
+// Difference between call, apply, and bind.
+// What is event delegation?
